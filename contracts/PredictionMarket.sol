89a1577a369ba37e7851b48f1101db5080f385b2
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-16 15:52:16 +0200

    cleanup

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -1,6 +1,6 @@
  // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
